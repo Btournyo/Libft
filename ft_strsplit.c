790652771ff0557f438d4ebffcb2ae35e229381c
Btournyo <43573063+Btournyo@users.noreply.github.com>
2018-12-04 10:56:21 +0100

    fini split!

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,95 +6,88 @@
 /*   By: btournyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 10:20:12 by btournyo          #+#    #+#             */
-/*   Updated: 2018/12/03 09:56:10 by btournyo         ###   ########.fr       */
+/*   Updated: 2018/12/04 10:53:11 by btournyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
-#include <stdio.h>
 
-char *ft_strcpy(char *dest, char *src)
+static void	ft_cpy(char *dest, char *src, size_t n)
 {
-	int i;
-	
+	size_t i;
+
 	i = 0;
-	while (src[i])
+	while (i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return (dest);
 }
 
-int check_sign(char a, char c)
+int	count_words(const char *str, char c)
 {
-	if (a == c)
-		return (1);
-	return (0);
-}
-
-int	count_words(char *str, char c)
-{
-	int i;
-	int words;
+	size_t i;
+	size_t words;
 
 	i = 0;
 	words = 0;
-	if (check_sign(str[0], c) == 1)
-		i++;
 	while (str[i])
 	{
-		if (check_sign(str[i], c) == 0  && check_sign(str[i + 1], c) == 1)
-			words++;
-		i++;
+		while (str[i] == c)
+			i++;
+		if (!str[i])
+			break ;
+		words++;
+		while (str[i] && str[i] != c)
+			i++;
 	}
 	return (words);
 }
 
-int	size_words(char *str, char c)
+static size_t split_words(const char *s, size_t n, char **tab, char c)
 {
-	int i;
+	size_t i;
+	size_t start;
+	size_t end;
 
 	i = 0;
-	while (str[i] && check_sign(str[i], c) == 0)
+	start = 0;
+	while (i < n && s[start])
+	{
+		while (s[start] == c)
+			start++;
+		end = start;
+		while (s[end] != c && s[end])
+			end++;
+		if (!(tab[i] = malloc(end - start + 1)))
+			return (0);
+		ft_cpy(tab[i], (char *)s + start, end - start);
+		start = end + 1;
 		i++;
-	return (i);
+	}
+	return (1);
 }
 
 char ** ft_strsplit(char const *s, char c)
 {
-	int 	i;
-	int		j;
+	size_t 	nb;
 	char	**tab;
 
-	i = 0;
-    j = 0;
-	if ((tab = (char **)malloc(sizeof(char *) * count_words((char *)s, c) + 1))== NULL)
+	if (!s)
 		return (NULL);
-	while (s[i] != 0)
-    {
-        if (!(check_sign(s[i], c)) && s[i])
-        {
-          if ((tab[j] = (char *)malloc(sizeof(char) * size_words((char *)&s[i], c) + 1)) == NULL)
-          return (NULL);
-          ft_strsub(tab[j], i, size_words((char *)&s[i], c));  
-          printf("%s", tab[j]);
-          j++;
-          i = i + size_words((char *)&s[i], c);
-        }
-    i++;
-  }
-tab[j] = 0;
-return (tab);
-}
-
-int	main()
-{
-    char c = '*';
-    char const *s = "*salut*les***etudiants*";
-
-    printf("%s", ft_strsplit(s, c));
-	return (0);
+	nb = count_words(s, c);
+	if (!nb)
+	{
+		if (!(tab = malloc(sizeof(*tab))))
+			return (NULL);
+		tab[0] = NULL;
+		return (tab);
+	}
+	if (!(tab = malloc(sizeof(*tab) * (nb + 1))))
+		return (NULL);
+	if (!split_words(s, nb, tab, c))
+		return (NULL);
+	tab[nb] = 0;
+	return (tab);
 }
